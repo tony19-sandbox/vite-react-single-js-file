1bbd3acb2f250aab3f9e740d89f27e74d11c3368
Tony Trinh <tony19@gmail.com>
2022-06-06 20:09:52 -0500

    split App into component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import logo from './logo.svg'
+import HelloWorld from './HelloWorld.jsx'
 import './App.css'
 
 function App() {
@@ -8,13 +8,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
+        <HelloWorld />
         <p>
           Edit <code>App.jsx</code> and save to test HMR updates.
         </p>
---
+++ src/HelloWorld.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './HelloWorld.css'
+
+function HelloWorld() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="HelloWorld">
+      <header className="HelloWorld-header">
+        <img src={logo} className="HelloWorld-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+
+      </header>
+    </div>
+  )
+}
+
+export default App
